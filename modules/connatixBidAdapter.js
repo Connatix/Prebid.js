c70897000b25e7835d6e95373cb7a3177bd0d40a
Rares Preda <rares.preda@connatix.com>
2023-07-06 10:03:03 +0300

    finished connatix adapter and unit tests

--- modules/connatixBidAdapter.js
+++ modules/connatixBidAdapter.js
@@ -4,30 +4,25 @@ import {
   isFn,
   logError,
   isArray
-  // logMessage
 } from '../src/utils.js';
 
 import {
   registerBidder
 } from '../src/adapters/bidderFactory.js';
 import {
-  config
-} from '../src/config.js';
-import {
   BANNER,
 } from '../src/mediaTypes.js';
 
 const BIDDER_CODE = 'connatix';
 const AD_URL = 'https://capi.connatix.com/rtb/hba';
-const SYNC_URL = 'https://placeholder.com/sync';
 const DEFAULT_MAX_TTL = '3600';
 const DEFAULT_CURRENCY = 'USD';
 
-/* Get the bid floor value from the bid object,
-  either using the getFloor function or by accessing the 'params.bidfloor' property.
-  If the bid floor cannot be determined, return 0 as a fallback value.
-*/
-function getBidFloor(bid) {
+/*
+ * Get the bid floor value from the bid object, either using the getFloor function or by accessing the 'params.bidfloor' property.
+ * If the bid floor cannot be determined, return 0 as a fallback value.
+ */
+export function getBidFloor(bid) {
   if (!isFn(bid.getFloor)) {
     return deepAccess(bid, 'params.bidfloor', 0);
   }
@@ -45,18 +40,19 @@ function getBidFloor(bid) {
   }
 }
 
-/*  Wrap the provided bid, playerId, customerId, and scriptId in an HTML string
-  that includes the Connatix player script and related data.
-  This HTML string will be used as the ad content.
+/*
+ * Wrap the provided bid, playerId, customerId, and scriptId in an HTML string
+ * that includes the Connatix player script and related data.
+ * This HTML string will be used as the ad content.
  */
-function wrapAd(lineItems, requestId, playerId, customerId, isInApp) {
+export function wrapAd(lineItems, requestId, playerId, customerId, isInApp) {
   var settings = {
     advertising: {
       standaloneLineItems: lineItems
     }
   };
   var scriptSrc = isInApp ? `//cd.connatix.com/connatix.player.omid.js?cid=${customerId}` : `//cd.connatix.com/connatix.player.js?cid=${customerId}`;
-  var ad = `<!DOCTYPE html>
+  return `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -68,28 +64,6 @@ function wrapAd(lineItems, requestId, playerId, customerId, isInApp) {
       <script id="${requestId}">(new Image()).src = 'https://capi.connatix.com/tr/si?token=${playerId}&cid=${customerId}';  cnx.cmd.push(function() {    cnx({      playerId: "${playerId}", settings: ${JSON.stringify(settings)} }).render("${requestId}");  });</script>
     </body>
   </html>`;
-
-  // !!!! Decomment this for testing purpose !!!!!
-
-  // Create an iframe element
-  var iframe = document.createElement('iframe');
-
-  // Set iframe attributes
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
-
-  // Add the iframe to the document or a specific container
-  document.body.appendChild(iframe);
-
-  // Get the iframe's document
-  var doc = iframe.contentDocument || iframe.contentWindow.document;
-
-  // Write the HTML string to the iframe document
-  doc.open();
-  doc.write(ad);
-  doc.close();
-
-  return ad;
 };
 
 export const spec = {
@@ -97,39 +71,41 @@ export const spec = {
   gvlid: 143,
   supportedMediaTypes: [BANNER],
 
-  /* Check if a bid request is valid by verifying if the bidId, params,
-   and placementId properties are present,
-   and if the mediaTypes object contains a BANNER object with a sizes property. */
+  /*
+   * Validate the bid request.
+   * If the request is valid, Connatix is trying to obtain at least one bid.
+   * Otherwise, the request to the Connatix server is not made
+   */
   isBidRequestValid: (bid = {}) => {
-    const {
-      params,
-      bidId,
-      mediaTypes
-    } = bid;
-    if (!(bidId && params && params.placementId)) {
-      return false;
-    };
+    const bidId = deepAccess(bid, 'bidId');
+    const mediaTypes = deepAccess(bid, 'mediaTypes', {})
+    const params = deepAccess(bid, 'params', {});
+    const bidder = deepAccess(bid, 'bidder');
 
-    return Boolean(mediaTypes[BANNER] && mediaTypes[BANNER].sizes);
+    const isValidBidder = (bidder === BIDDER_CODE);
+    const hasBidId = Boolean(bidId);
+    const hasSizes = mediaTypes[BANNER] ? Boolean(mediaTypes[BANNER].sizes) : false;
+    const hasRequiredBidParams = Boolean(params.placementId);
+
+    const isValid = isValidBidder && hasBidId && hasSizes && hasRequiredBidParams;
+    if (!isValid) {
+      logError(`Invalid bid request: isValidBidder: ${isValidBidder} hasBidId: ${hasBidId}, hasSizes: ${hasSizes}, hasRequiredBidParams: ${hasRequiredBidParams}`);
+    }
+    return isValid;
   },
 
-  /* Build the request payload by processing valid bid requests and extracting the necessary information.
-  Determine the host and page from the bidderRequest's refferUrl, and include ccpa and gdpr consents.
-  Return an object containing the request method, url, and the constructed payload. */
+  /*
+   * Build the request payload by processing valid bid requests and extracting the necessary information.
+   * Determine the host and page from the bidderRequest's refferUrl, and include ccpa and gdpr consents.
+   * Return an object containing the request method, url, and the constructed payload.
+   */
   buildRequests: (validBidRequests = [], bidderRequest = {}) => {
-    // let refferLocation;
-    // try {
-    //   refferLocation = bidderRequest.refferUrl && new URL(bidderRequest.refferUrl);
-    // } catch (e) {
-    //   logMessage(e);
-    // }
-
     const bidRequests = validBidRequests.map(bid => {
       const {
         bidId,
         mediaTypes,
         params,
-        sizes
+        sizes,
       } = bid;
       return {
         bidId,
@@ -144,12 +120,9 @@ export const spec = {
       ortb2: bidderRequest.ortb2,
       gdprConsent: bidderRequest.gdprConsent,
       uspConsent: bidderRequest.uspConsent,
-      refererInfo: {
-        ...bidderRequest.refererInfo,
-        ref: bidderRequest.page
-      },
+      refererInfo: bidderRequest.refererInfo,
       bidRequests,
-    }
+    };
 
     return {
       method: 'POST',
@@ -158,16 +131,13 @@ export const spec = {
     };
   },
 
-  /* Interpret the server response and create an array of bid responses by extracting and formatting
-  relevant information such as requestId, cpm, width, height, creativeId,
-  and ad content (wrapped using the wrapAd function). */
+  /*
+   * Interpret the server response and create an array of bid responses by extracting and formatting
+   * relevant information such as requestId, cpm, width, height, creativeId,
+   * and ad content (wrapped using the wrapAd function).
+   * Returns an array of bid responses by extracting and formatting the server response
+   */
   interpretResponse: (serverResponse) => {
-    const { PlayerId, CustomerId, Bids } = serverResponse.body;
-    if (!PlayerId || !CustomerId || !Bids) {
-      var serverResponseJSONString = '{"body":{"PlayerId":"e4984e88-9ff4-45a3-8b9d-33aabcad634e","CustomerId":"99f20d18-c4b4-4a28-8d8e-d43e2c8cb4ac","Bids":[{"RequestId":"21cb54857820cd","Cpm":8,"Ttl":86400,"LineItems":[{"LineItem":{"PublisherLineItemId":"81199cb2-99c9-4b26-9b9a-37efe8821977","AdvertiserLineItemId":"d09815c9-6e65-43c8-984a-f4ad178ab5b5","Url":null,"Type":2,"RequireSoundOn":false,"RequireViewability":false,"RemoveOutOfFocus":false,"GdprvId":"12","Trackers":null,"Duration":0,"Renditions":null,"MediaCreativeId":null,"IgnoreFilledRequests":true,"RequestsCap":1,"ImpressionsCap":null,"Priority":1,"PbjsTimeout":1000,"MediaCreativeClickUrl":null,"RequestsCapTimeframe":0,"ImpressionsCapTimeframe":0,"CapType":0,"CreativeHlsFileName":null,"TimeBetweenRequests":null,"MediaTargeting":null,"MediaKeywordsTargeting":null,"MediaLanguageTargeting":null,"AdType":0,"OverlayTime":null,"FloorPrice":8,"DemandPartnerInfo":{"Name":"Beeswax","DealId":"Connatix-BPM-TheBlackTux-Weddings","BuyersId":null,"TagId":null,"PublisherId":null,"BaseDemandPartnerName":null,"ExcludePmpObject":false,"NetworkExpiryTime":86400},"AdBreakSettings":null,"OutOfViewPauseType":0,"MoatTrackingEnabled":false,"CreativeId":"7c8df0c1-ff37-42ab-8270-77286c330553","EnableVerticalAd":false,"BlockedDomainsRevisionNumber":null,"AdvertiserContextualTargeting":[{"Filters":[{"Type":0,"Level":0,"HashedItemsIds":["d606168ba6d66655ea51effc1e6aca6c","74495cb3cc8737ce61e22520fb318caa","d4594abaaa3bc931da08158994cf9e9a","508fbedb028c2c1884502294ed328a93","e340dc83bc6529e84bbc7d2d4a994de1","a3dc95b127a7c0cc2cdf03c46f61e1f3","24193c467467ff2f16367f8ec8a7fd5f"]}]},{"Filters":[{"Type":0,"Level":1,"HashedItemsIds":["d606168ba6d66655ea51effc1e6aca6c","74495cb3cc8737ce61e22520fb318caa","d4594abaaa3bc931da08158994cf9e9a","508fbedb028c2c1884502294ed328a93","e340dc83bc6529e84bbc7d2d4a994de1","a3dc95b127a7c0cc2cdf03c46f61e1f3","24193c467467ff2f16367f8ec8a7fd5f"]}]}],"PublisherContextualTargeting":null,"PlayerSizeTargetings":null,"AmazonDealId":null,"PlayerOrientationTargeting":0,"SkipMin":6,"Skippability":0,"AdOpportunityForceCalling":false,"NetworkExpiryTime":86400,"NonlinearLineItemSettings":null,"NewFloorPrices":{"FloorPrices":[],"Recommended":0},"PrebidParams":null,"IsPmp":true,"QueryJsTargeting":[],"AdxInfoModel":null,"IntentIQ":null,"GrossNetPercentageDifference":null,"IIQSource":"Beeswax"},"Bids":[{"WinNoticeUrl":null,"AdQualityCheckUrl":null,"Content":"<VAST version=\'3.0\'><Ad id=\'3919\'><Wrapper><Error><![CDATA[https://us-east-1.event.prod.bidr.io/log/vasterror?error_event=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGM8eIBQyA2N0eA==&error_code=[ERRORCODE]]]></Error><AdSystem>Beeswax</AdSystem><VASTAdTagURI><![CDATA[https://vast.extremereach.io/vast?line_item=15798525&subid1=novpaid&er_pm=&er_ar=0&us_privacy=${US_PRIVACY}&er_did=&ba_cb=385236447]]></VASTAdTagURI><Impression><![CDATA[https://us-east-1.event.prod.bidr.io/log/imp/ctx?sie=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggwD4oAWAAeM8egAEUkgEDY3R4oAHsgAOoAQDCAQDKAR9ibGlzc3BvaW50LTk1ODg3YTAxNDM0OWMzYWM4Y2Q10gEjQklEX1JFUVVFU1RfUExBVEZPUk1fREVWSUNFX1RZUEU6UEPaAQ4yMDIzMDYxNTA0MDAwMOIBHhIECAUQARIECAIQARIECAMQARIECAQQARIECAEQAeoBF2lwLjo6ZmZmZjoxOC4xOTAuMTU2LjEy-gEVDccSYj8SDvgEAQEAAAAAAAAAAAAA&wp=8&fie=IMA-ShAYgJvuAkIHYmVlc3dheEgBaggIARAAGAAgAHDAPooBEAoFCMA-EAERAAAAAAAA8D_yAQIYAA==&]]></Impression><Creatives><Creative><Linear><TrackingEvents><Tracking event=\'start\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggAUDPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'firstQuartile\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggAkDPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'midpoint\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggA0DPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'thirdQuartile\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggBEDPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'complete\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggBUDPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'mute\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggB0DPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'unmute\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggCEDPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'pause\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggCUDPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'resume\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggCkDPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'fullscreen\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggC0DPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking><Tracking event=\'close\'><![CDATA[https://us-east-1.event.prod.bidr.io/log/act/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHggDEDPHkgUUgNjdHhgAHoeEgQIARABEgQIAhABEgQIBBABEgQIAxABEgQIBRAB&]]></Tracking></TrackingEvents><VideoClicks><ClickTracking><![CDATA[https://us-east-1.event.prod.bidr.io/log/clk/ctx?ai=ChIIiuCu7wUQrS4Yq4rz17bF_wISFAoKYmxpc3Nwb2ludBACGOERILchGgNjdHg4zx5AFEgBUgNjdHhgAHoeEgQIAhABEgQIBBABEgQIAxABEgQIBRABEgQIARAB&audit_flag_wp=8]]></ClickTracking></VideoClicks></Linear></Creative></Creatives><Extensions></Extensions></Wrapper></Ad></VAST>","EncryptedPrice":"BfD+jrjeEewG9y8zQrSv4hUvCpLcSj3F6DP5VC1LrBU=","CreativeId":"blisspoint-3919","SeatId":"beeswax","Adomain":"theblacktux.com","SyncedUser":false,"RequestedFloorPrice":8,"CampaignId":"2273","Width":0,"Height":0,"BillingNoticeUrls":[],"Mime":null,"BidPrice":-37,"DspId":null,"CnxBidId":"3e0c8f5c-6992-48b7-a603-14d5b962f217","LogAuctionInformation":false,"NumberOfBids":1,"NumberOfSeatBids":1,"RequestTime":32,"DealId":"Connatix-BPM-TheBlackTux-Weddings","AuctionType":1,"AuctionBidId":"CIrgru8FEK0uGKuK89e2xf8C","BidId":"beeswax/blisspoint","FloorPriceInformation":{"FloorPrices":[8],"FloorPriceIndexUsed":0,"FloorPriceFactorBypassReason":0,"AdxPredictedFloorPrice":null},"Discount":null,"LiveRamp":0,"ServerBillingNoticeUrls":null,"IIQData":{"EIDs":{},"ABGroup":0,"IIQUserSync":null}}]}]}]},"headers":{}}';
-      serverResponse = JSON.parse(serverResponseJSONString);
-    }
-
     const responseBody = serverResponse.body;
     const bids = responseBody.Bids;
     const playerId = responseBody.PlayerId;
@@ -185,7 +155,6 @@ export const spec = {
       currency: 'USD',
       mediaType: BANNER,
       netRevenue: true,
-      // TODO: check if we'll get netRevenue from the server
       width: bidResponse.width,
       height: bidResponse.height,
       creativeId: bidResponse.creativeId,
@@ -196,32 +165,15 @@ export const spec = {
     return bidResponses;
   },
 
-  /* Determine the user sync type (either 'iframe' or 'image') based on syncOptions.
-   Construct the sync URL by appending required query parameters such as gdpr, ccpa, and coppa consents.
-   Return an array containing an object with the sync type and the constructed URL. */
-  getUserSyncs: (syncOptions, serverResponses, gdprConsent, uspConsent) => {
-    let syncType = syncOptions.iframeEnabled ? 'iframe' : 'image';
-    let syncUrl = SYNC_URL + `/${syncType}?pbjs=1`;
-    if (gdprConsent && gdprConsent.consentString) {
-      if (typeof gdprConsent.gdprApplies === 'boolean') {
-        syncUrl += `&gdpr=${Number(gdprConsent.gdprApplies)}&gdpr_consent=${gdprConsent.consentString}`;
-      } else {
-        syncUrl += `&gdpr=0&gdpr_consent=${gdprConsent.consentString}`;
-      }
-    }
-    if (uspConsent && uspConsent.consentString) {
-      syncUrl += `&ccpa_consent=${uspConsent.consentString}`;
-    }
-
-    const coppa = config.getConfig('coppa') ? 1 : 0;
-    syncUrl += `&coppa=${coppa}`;
-
-    return [{
-      type: syncType,
-      url: syncUrl
-    }];
+  /*
+   * Determine the user sync type (either 'iframe' or 'image') based on syncOptions.
+   * Construct the sync URL by appending required query parameters such as gdpr, ccpa, and coppa consents.
+   * Return an array containing an object with the sync type and the constructed URL.
+   * NOTE: We don't do user sync for now
+   */
+  getUserSyncs: (syncOptions, serverResponses, gdprConsent, uspConsent, gppConsent) => {
+    return [];
   }
-
 };
 
 registerBidder(spec);
---
+++ test/spec/modules/connatixBidAdapter_spec.js
@@ -0,0 +1,270 @@
+import { expect } from 'chai';
+import { spec, getBidFloor as connatixGetBidFloor } from '../../../modules/connatixBidAdapter.js';
+import { BANNER } from '../../../src/mediaTypes.js';
+
+describe('connatixBidAdapter', function () {
+  let bid;
+
+  function mockBidRequest() {
+    const mediaTypes = {
+      banner: {
+        sizes: [16, 9],
+      }
+    };
+    return {
+      bidId: 'testing',
+      bidder: 'connatix',
+      params: {
+        placementId: '30e91414-545c-4f45-a950-0bec9308ff22'
+      },
+      mediaTypes
+    };
+  };
+
+  describe('isBidRequestValid', function () {
+    this.beforeEach(function () {
+      bid = mockBidRequest();
+    });
+
+    it('Should return true if all required fileds are present', function () {
+      expect(spec.isBidRequestValid(bid)).to.be.true;
+    });
+    it('Should return false if bidder does not correspond', function () {
+      bid.bidder = 'abc';
+      expect(spec.isBidRequestValid(bid)).to.be.false;
+    });
+    it('Should return false if bidId is missing', function () {
+      delete bid.bidId;
+      expect(spec.isBidRequestValid(bid)).to.be.false;
+    });
+    it('Should return false if params object is missing', function () {
+      delete bid.params;
+      expect(spec.isBidRequestValid(bid)).to.be.false;
+    });
+    it('Should return false if placementId is missing from params', function () {
+      delete bid.params.placementId;
+      expect(spec.isBidRequestValid(bid)).to.be.false;
+    });
+    it('Should return false if mediaTypes is missing', function () {
+      delete bid.mediaTypes;
+      expect(spec.isBidRequestValid(bid)).to.be.false;
+    });
+    it('Should return false if banner is missing from mediaTypes ', function () {
+      delete bid.mediaTypes.banner;
+      expect(spec.isBidRequestValid(bid)).to.be.false;
+    });
+    it('Should return false if sizes is missing from banner object', function () {
+      delete bid.mediaTypes.banner.sizes;
+      expect(spec.isBidRequestValid(bid)).to.be.false;
+    });
+    it('Should return true if add an extra field was added to the bidRequest', function () {
+      bid.params.test = 1;
+      expect(spec.isBidRequestValid(bid)).to.be.true;
+    });
+  });
+
+  describe('buildRequests', function () {
+    let serverRequest;
+    let bidderRequests = {
+      refererInfo: {
+        canonicalUrl: '',
+        numIframes: 0,
+        reachedTop: true,
+        referer: 'http://example.com',
+        stack: ['http://example.com']
+      },
+      gdprConsent: {
+        consentString: 'BOJ/P2HOJ/P2HABABMAAAAAZ+A==',
+        vendorData: {},
+        gdprApplies: true
+      },
+      uspConsent: '1YYY',
+      ortb2: {
+        site: {
+          data: {
+            pageType: 'article'
+          }
+        }
+      }
+    };
+
+    this.beforeEach(function () {
+      bid = mockBidRequest();
+      serverRequest = spec.buildRequests([bid], bidderRequests);
+    })
+
+    it('Creates a ServerRequest object with method, URL and data', function () {
+      expect(serverRequest).to.exist;
+      expect(serverRequest.method).to.exist;
+      expect(serverRequest.url).to.exist;
+      expect(serverRequest.data).to.exist;
+    });
+    it('Returns GET method', function () {
+      expect(serverRequest.method).to.equal('POST');
+    });
+    it('Returns valid URL', function () {
+      expect(serverRequest.url).to.equal('https://capi.connatix.com/rtb/hba');
+    });
+    it('Returns request payload', function () {
+      expect(serverRequest.data).to.not.empty;
+    });
+    it('Validate request payload', function () {
+      expect(serverRequest.data.bidRequests[0].bidId).to.equal(bid.bidId);
+      expect(serverRequest.data.bidRequests[0].placementId).to.equal(bid.params.placementId);
+      expect(serverRequest.data.bidRequests[0].floor).to.equal(0);
+      expect(serverRequest.data.bidRequests[0].mediaTypes).to.equal(bid.mediaTypes);
+      expect(serverRequest.data.bidRequests[0].sizes).to.equal(bid.mediaTypes.sizes);
+      expect(serverRequest.data.refererInfo).to.equal(bidderRequests.refererInfo);
+      expect(serverRequest.data.gdprConsent).to.equal(bidderRequests.gdprConsent);
+      expect(serverRequest.data.uspConsent).to.equal(bidderRequests.uspConsent);
+      expect(serverRequest.data.ortb2).to.equal(bidderRequests.ortb2);
+    });
+  });
+
+  describe('getUserSyncs', function() {
+    it('Returns always an empty array because we do not do user sync for now', function () {
+      expect(spec.getUserSyncs({}, [], {}, {}, {})).to.be.an('array').that.is.empty;
+    });
+  });
+
+  describe('interpretResponse', function () {
+    const CustomerId = '99f20d18-c4b4-4a28-3d8e-d43e2c8cb4ac';
+    const PlayerId = 'e4984e88-9ff4-45a3-8b9d-33aabcad634f';
+    const Bid = {Cpm: 0.1, LineItems: [], RequestId: '2f897340c4eaa3', Ttl: 86400};
+
+    let serverResponse;
+    this.beforeEach(function () {
+      serverResponse = {
+        body: {
+          CustomerId,
+          PlayerId,
+          Bids: [ Bid ]
+        },
+        headers: function() { }
+      };
+    });
+
+    it('Should return an empty array if Bids is null', function () {
+      serverResponse.body.Bids = null;
+
+      const response = spec.interpretResponse(serverResponse);
+      expect(response).to.be.an('array').that.is.empty;
+    });
+
+    it('Should return an empty array if Bids is empty array', function () {
+      serverResponse.body.Bids = [];
+      const response = spec.interpretResponse(serverResponse);
+      expect(response).to.be.an('array').that.is.empty;
+    });
+
+    it('Should return an empty array if CustomerId is null', function () {
+      serverResponse.body.CustomerId = null;
+      const response = spec.interpretResponse(serverResponse);
+      expect(response).to.be.an('array').that.is.empty;
+    });
+
+    it('Should return an empty array if PlayerId is null', function () {
+      serverResponse.body.PlayerId = null;
+      const response = spec.interpretResponse(serverResponse);
+      expect(response).to.be.an('array').that.is.empty;
+    });
+
+    it('Should return one bid response for one bid', function() {
+      const bidResponses = spec.interpretResponse(serverResponse);
+      expect(bidResponses.length).to.equal(1);
+
+      const [ bidResponse ] = bidResponses;
+      expect(bidResponse.requestId).to.equal(serverResponse.body.Bids[0].RequestId);
+      expect(bidResponse.cpm).to.equal(serverResponse.body.Bids[0].Cpm);
+      expect(bidResponse.ttl).to.equal(serverResponse.body.Bids[0].Ttl);
+      expect(bidResponse.currency).to.equal('USD');
+      expect(bidResponse.mediaType).to.equal(BANNER);
+      expect(bidResponse.netRevenue).to.be.true;
+    });
+
+    it('Should contains the same values as in the serverResponse', function() {
+      const bidResponses = spec.interpretResponse(serverResponse);
+
+      const [ bidResponse ] = bidResponses;
+      expect(bidResponse.requestId).to.equal(serverResponse.body.Bids[0].RequestId);
+      expect(bidResponse.cpm).to.equal(serverResponse.body.Bids[0].Cpm);
+      expect(bidResponse.ttl).to.equal(serverResponse.body.Bids[0].Ttl);
+      expect(bidResponse.currency).to.equal('USD');
+      expect(bidResponse.mediaType).to.equal(BANNER);
+      expect(bidResponse.netRevenue).to.be.true;
+    });
+
+    it('Should return n bid responses for n bids', function() {
+      const serverResponse = {
+        body: {
+          CustomerId,
+          PlayerId,
+          Bids: [ { ...Bid }, { ...Bid } ]
+        },
+        headers: function() { }
+      };
+
+      const firstBidCpm = 4;
+      serverResponse.body.Bids[0].Cpm = firstBidCpm;
+
+      const secondBidCpm = 13;
+      serverResponse.body.Bids[1].Cpm = secondBidCpm;
+
+      const bidResponses = spec.interpretResponse(serverResponse);
+      expect(bidResponses.length).to.equal(2);
+
+      expect(bidResponses[0].cpm).to.equal(firstBidCpm);
+      expect(bidResponses[1].cpm).to.equal(secondBidCpm);
+    });
+  })
+
+  describe('getBidFloor', function () {
+    this.beforeEach(function () {
+      bid = mockBidRequest();
+    });
+
+    it('Should return 0 if both getFloor method and bidfloor param from bid are absent.', function () {
+      const floor = connatixGetBidFloor(bid);
+      expect(floor).to.equal(0);
+    });
+
+    it('Should return the value of the bidfloor parameter if the getFloor method is not defined but the bidfloor parameter is defined', function () {
+      const floorValue = 3;
+      bid.params.bidfloor = floorValue;
+
+      const floor = connatixGetBidFloor(bid);
+      expect(floor).to.equal(floorValue);
+    });
+
+    it('Should return the value of the getFloor method if the getFloor method is defined but the bidfloor parameter is not defined', function () {
+      const floorValue = 7;
+      bid.getFloor = function() {
+        return { floor: floorValue };
+      };
+
+      const floor = connatixGetBidFloor(bid);
+      expect(floor).to.equal(floorValue);
+    });
+
+    it('Should return the value of the getFloor method if both getFloor method and bidfloor parameter are defined', function () {
+      const floorParamValue = 3;
+      bid.params.bidfloor = floorParamValue;
+
+      const floorMethodValue = 7;
+      bid.getFloor = function() {
+        return { floor: floorMethodValue };
+      };
+
+      const floor = connatixGetBidFloor(bid);
+      expect(floor).to.equal(floorMethodValue);
+    });
+
+    it('Should return 0 if the getFloor method is defined and it crash when call it', function () {
+      bid.getFloor = function() {
+        throw new Error('error');
+      };
+      const floor = connatixGetBidFloor(bid);
+      expect(floor).to.equal(0);
+    });
+  })
+});
